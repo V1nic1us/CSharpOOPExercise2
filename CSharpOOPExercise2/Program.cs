aa8d8aabb8340e5df1dbf4564e327d4f16118ff5
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-10-31 23:02:11 -0400

    Adicionar arquivos de projeto.

---
+++ CSharpOOPExercise2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharpOOPExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de quartos alugados");
+            int quantityRooms = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Room[] rooms = new Room[10];
+
+            for (int i = 0; i < quantityRooms; i++)
+            {
+                Console.WriteLine("Digite o Nome");
+                string name = Console.ReadLine();
+                Console.WriteLine("Digite o Email");
+                string email = Console.ReadLine();
+                Console.WriteLine("Digite o numero do quarto");
+                int numberRoom = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Room room = new Room(name, email, numberRoom);
+                for (int j = 0; j <= quantityRooms; j++)
+                {
+                    if (j == room.numberRoom)
+                    {
+                        rooms[j] = room;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (rooms[i] != null)
+                {
+                    Console.WriteLine(rooms[i].ToString());
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ CSharpOOPExercise2/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOPExercise2
+{
+    public class Room
+    {
+        public string name {  get; set; }
+        public string email {  get; set; }
+        public int numberRoom { get; set; }
+
+        public Room(string name, string email, int numberRoom)
+        {
+            this.name = name;
+            this.email = email;
+            this.numberRoom = numberRoom;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {name}, Email {email}, Quarto: {numberRoom}";
+        }
+    }
+}
